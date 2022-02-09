406af502f5f01b48f63d92095fc7a2bb2f74424a
jooeun kim <jay311311@gmail.com>
2022-02-09 21:43:51 +0900

    UILabel _ 음악 전체 시간  00:00 형식으로 표현하는 로직 구현

--- iOS_MusicPlayer/ViewController.swift
+++ iOS_MusicPlayer/ViewController.swift
@@ -10,26 +10,27 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var playBtn : UIButton?
-    @IBOutlet weak var playTime : UILabel?
-    @IBOutlet weak var playAllTime : UILabel?
-    @IBOutlet weak var playSlider : UISlider?
+    @IBOutlet weak var playBtn : UIButton!
+    @IBOutlet weak var playTime : UILabel!
+    @IBOutlet weak var playAllTime : UILabel!
+    @IBOutlet weak var playSlider : UISlider!
     
-    var url : URL?
+    var url = Bundle.main.url(forResource: "sound", withExtension: "mp3")
     var audioPlayer : AVAudioPlayer?
     var playState : Bool?
+    var playDuration : Double?
+    var playingAllTime:String?
+
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         loadPlayer()
-        
         // Do any additional setup after loading the view.
     }
     
     func loadPlayer (){
         // AVAudioPlayer 에 url 넣으려면 do catch 문 안에서 try throw 를 사용해야한다
-        url = Bundle.main.url(forResource: "sound", withExtension: "mp3")
+   
         
         if let urlFile = url {
             do{
@@ -37,26 +38,48 @@ class ViewController: UIViewController {
                 print("ready to play")
                 audioPlayer?.play()
                 playBtn?.isSelected = true
+                playDuration = audioPlayer?.duration
+                guard let durationTime = playDuration else { return }
+                playingAllTime = showPlayingSecond(durationTime)
+                playAllTime.text = playingAllTime
+                
+               // playCurrent = audioPlayer?.currentTime
+                
+//                playCurrent = audioPlayer?.currentTime
+//                guard let currentTime = playCurrent else { return }
+//                playingCurrentTime = showPlayingSecond(currentTime)
+//                playTime.text = playingCurrentTime
+   
             }catch let error as NSError{
                 print("i made \(error)")
             }
         }
+        
+       
+
     }
+    
+    func showPlayingSecond (_ durations : Double) -> String{
+        let min = Int(durations / 60)
+        let sec  = Int(durations)
+        let playingTime: String = "\(String(format: "%02d", min)):\(String(format: "%02d", sec))"
+        return playingTime
+    }
+    
+    
+    
+    
     
     
     @IBAction func touchPlayBtn(_ sender: UIButton) -> Void {
         print("touch playBtn")
-        guard let playState = audioPlayer?.isPlaying  else { return }
-        
-        if playState  {
+       
+        sender.isSelected = !sender.isSelected
+        if !sender.isSelected  {
             audioPlayer?.pause()
-            playBtn?.isSelected = false
         }else{
             audioPlayer?.play()
-            playBtn?.isSelected = true
         }
-        
-        
     }
     
     
@@ -65,4 +88,5 @@ class ViewController: UIViewController {
     }
     
 }
+
 
